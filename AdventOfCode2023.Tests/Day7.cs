0995a4007a53f5dafd8917b6597337eb07e9f69f
Nick <nmurphy@neworbit.co.uk>
2023-12-11 17:02:52 +0000

    fixing tests, adding more for math helpers

---
+++ AdventOfCode2023.Tests/Day7.cs
@@ -0,0 +1,100 @@
+﻿using AdventOfCode2023.Day7;
+using AdventOfCode2023.Day7.Models;
+namespace AdventOfCode2023.Tests
+{
+    [TestClass]
+    public class Day7Tests
+    {
+        [TestMethod]
+        public void Part1Example()
+        {
+            Assert.AreEqual(6440, Day7.Day7.Part1(example.Split(Environment.NewLine)));
+        }
+
+        [TestMethod]
+        public void CompareTwo()
+        {
+            var hand1 = new CardHand("T55J5", 0);
+            var hand2 = new CardHand("KK677", 0);
+            var comparer = new CardComparer();
+            Assert.IsTrue(comparer.Compare(hand1, hand2) > 0);
+        }
+
+        [TestMethod]
+        public void CompareEqual()
+        {
+            var hand1 = new CardHand("T55J5", 0);
+            var hand2 = new CardHand("T55J5", 0);
+            var comparer = new CardComparer();
+            Assert.AreEqual(0, comparer.Compare(hand1, hand2));
+        }
+
+        [TestMethod]
+        public void CompareSameType()
+        {
+            var hand1 = new CardHand("T55J5", 0);
+            var hand2 = new CardHand("KKK67", 0);
+            var comparer = new CardComparer();
+            Assert.IsTrue(comparer.Compare(hand1, hand2) < 0);
+        }
+
+        [TestMethod]
+        public void CompareSameTypeLastDiffers()
+        {
+            var hand1 = new CardHand("KKK65", 0);
+            var hand2 = new CardHand("KKK67", 0);
+            var comparer = new CardComparer();
+            Assert.IsTrue(comparer.Compare(hand1, hand2) < 0);
+        }
+
+        [TestMethod]
+        public void CompareWildcard()
+        {
+            var hand1 = new CardHand("KKK6J", 0, true);
+            var hand2 = new CardHand("KKK67", 0, true);
+            var comparer = new CardComparer();
+            Assert.IsTrue(comparer.Compare(hand1, hand2) > 0);
+        }
+
+        [TestMethod]
+        public void JokerIsWeakest()
+        {
+            var hand1 = new CardHand("KKJ67", 0, true);
+            var hand2 = new CardHand("KK2K7", 0, true);
+            var comparer = new CardComparer();
+            Assert.IsTrue(comparer.Compare(hand1, hand2) < 0);
+        }
+
+        [TestMethod]
+        public void JokerMakesFive()
+        {
+            var hand = new CardHand("KKJKK", 0, true);
+            Assert.AreEqual(HandType.FiveOfAKind, hand.HandType);
+        }
+
+        [DataTestMethod]
+        [DataRow("JJJJJ", HandType.FiveOfAKind)]
+        [DataRow("JJJJK", HandType.FiveOfAKind)]
+        [DataRow("JJJKK", HandType.FiveOfAKind)]
+        [DataRow("JJKKK", HandType.FiveOfAKind)]
+        [DataRow("JKKKK", HandType.FiveOfAKind)]
+        [DataRow("KKKKK", HandType.FiveOfAKind)]
+        [DataRow("JJJK6", HandType.FourOfAKind)]
+        [DataRow("JJJK6", HandType.FourOfAKind)]
+        [DataRow("JJKK6", HandType.FourOfAKind)]
+        [DataRow("JKKK6", HandType.FourOfAKind)]
+        [DataRow("KKKK6", HandType.FourOfAKind)]
+        [DataRow("KKK76", HandType.ThreeOfAKind)]
+        public void JokerActsAsWildcard(string input, HandType type)
+        {
+            var hand = new CardHand(input, 0, true);
+            Assert.AreEqual(type, hand.HandType);
+        }
+
+        private const string example = @"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+    }
+}
---
+++ AdventOfCode2023.Tests/MathHelpersTests.cs
@@ -0,0 +1,53 @@
+﻿using AdventOfCode2023.Helpers;
+
+namespace AdventOfCode2023.Tests
+{
+    [TestClass]
+    public class MathHelpersTests
+    {
+        [TestMethod]
+        public void CanFindPrimeFactors()
+        {
+            var result = MathHelpers.PrimeDivisors(2);
+            Assert.AreEqual(1, result.Count());
+            Assert.AreEqual(2, result.First());
+        }
+
+        [TestMethod]
+        public void CanFindPrimeFactorsOf6()
+        {
+            var result = MathHelpers.PrimeDivisors(6);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual(2, result.First());
+            Assert.AreEqual(3, result.Last());
+        }
+
+        [TestMethod]
+        public void CanFindPrimeFactorsOf9()
+        {
+            var result = MathHelpers.PrimeDivisors(9);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual(3, result.First());
+            Assert.AreEqual(3, result.Last());
+        }
+
+        [TestMethod]
+        public void CanFindPrimeFactorsOf11567()
+        {
+            var result = MathHelpers.PrimeDivisors(11567);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual(43, result.First());
+            Assert.AreEqual(269, result.Last());
+        }
+
+        [TestMethod]
+        public void CanFindPrimeFactorsOf19637()
+        {
+            var result = MathHelpers.PrimeDivisors(19637);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual(73, result.First());
+            Assert.AreEqual(269, result.Last());
+        }
+
+    }
+}
